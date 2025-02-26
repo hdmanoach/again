ee90530b0822c985431a105063ce198f83a70167
Manoach HOSSOU DODO <manoachdodo@gmail.com>
2025-02-26 22:54:23 +0100

    Ajout de la fonction multiply(a, b)

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+   def add(a, b):
+       return a + b
+
+   def multiply(a, b):
+       return a * b
+
+   print("Hello, Git!")
+   print("La somme de 3 et 4 est :", add(3, 4))
+   print("Le produit de 3 et 4 est :", multiply(3, 4))
